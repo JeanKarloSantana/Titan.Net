b1cef47d36283689163f71446767591ed60fdd56
jeankarlo_santana@bhd.com.do <jeankarlo_santana@bhd.com.do>
2024-12-11 23:48:29 -0400

    chore: remove comments

--- Titan.Api/Program.cs
+++ Titan.Api/Program.cs
@@ -2,10 +2,8 @@ using Titan.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
